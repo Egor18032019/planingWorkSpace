52bec22d6033d2f870f16b329467dbf2e2d4aa21
Egor <goro4@mail.ru>
2020-09-02 18:04:30 +0500

    Не мсог настоить gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // файл сборки (бандл) назвали bundle.js;
     filename: `bundle.js`,
     // eslint-disable-next-line no-undef
-    path: path.join(__dirname, `public`)// абсолютный путь до public
+    path: path.join(__dirname, `src`)// абсолютный путь до public
   },
   devServer: {
     // eslint-disable-next-line no-undef
